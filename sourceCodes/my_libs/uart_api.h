3dc4468ca4225cfa2bead5550ddba00bfe39cf27
Trieu Huynh Pham Nhat <86428744+trieu1162000@users.noreply.github.com>
2023-10-27 02:01:15 +0700

    Delete sourceCodes directory

--- sourceCodes/my_libs/uart_api.h
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
-#include "driverlib/gpio.h"
-#include "driverlib/pin_map.h"
-#include "driverlib/uart.h"
-
-extern void configureUART(void);
-extern void UARTStringPut(uint32_t, const char); 
