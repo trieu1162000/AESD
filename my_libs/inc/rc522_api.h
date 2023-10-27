fece14a20dacdeb6d99c3d3863ad7d5b51a00eaf
Trieu Huynh Pham Nhat <hpntrieu@tma.com.vn>
2023-10-27 15:15:13 +0700

    Second commit

--- my_libs/inc/rc522_api.h
+++ my_libs/inc/rc522_api.h
@@ -3,34 +3,37 @@
 
 #include "../inc/config_peripherals_api.h"
 
-// Status enumeration, Used with most functions
-#define MI_OK                                                0
-#define MI_NOTAGERR                                         (-1)
-#define MI_ERR                                              (-2)
-
-// RC522 Commands
-#define PCD_IDLE                                            0x00       // NO action; Cancel the current command
-#define PCD_AUTHENT                                         0x0E       // Authentication Key
-#define PCD_RECEIVE                                         0x08       // Receive Data
-#define PCD_TRANSMIT                                        0x04       // Transmit data
-#define PCD_TRANSCEIVE                                      0x0C       // Transmit and receive data,
-#define PCD_RESETPHASE                                      0x0F       // Reset
-#define PCD_CALCCRC                                         0x03       // CRC Calculate
-
-// Mifare_One card command word
-#define PICC_REQIDL                                         0x26       // find the antenna area does not enter hibernation
-#define PICC_REQALL                                         0x52       // find all the cards antenna area
-#define PICC_ANTICOLL                                       0x93       // anti-collision
-#define PICC_SELECTTAG                                      0x93       // election card
-#define PICC_AUTHENT1A                                      0x60       // authentication key A
-#define PICC_AUTHENT1B                                      0x61       // authentication key B
-#define PICC_READ                                           0x30       // Read Block
-#define PICC_WRITE                                          0xA0       // write block
-#define PICC_DECREMENT                                      0xC0       // debit
-#define PICC_INCREMENT                                      0xC1       // recharge
-#define PICC_RESTORE                                        0xC2       // transfer block data to the buffer
-#define PICC_TRANSFER                                       0xB0       // save the data in the buffer
-#define PICC_HALT                                           0x50       // Sleep
+#define LOW_PIN                                           0x00
+#define HIGH_PIN                                          0XFF
+    
+// Status enumeration, Used with most functions 
+#define MI_OK                                              0
+#define MI_NOTAGERR                                       (-1)
+#define MI_ERR                                            (-2)
+    
+// RC522 Commands   
+#define PCD_IDLE                                          0x00       // NO action; Cancel the current command
+#define PCD_AUTHENT                                       0x0E       // Authentication Key
+#define PCD_RECEIVE                                       0x08       // Receive Data
+#define PCD_TRANSMIT                                      0x04       // Transmit data
+#define PCD_TRANSCEIVE                                    0x0C       // Transmit and receive data,
+#define PCD_RESETPHASE                                    0x0F       // Reset
+#define PCD_CALCCRC                                       0x03       // CRC Calculate
+    
+// Mifare_One card command word 
+#define PICC_REQIDL                                       0x26       // find the antenna area does not enter hibernation
+#define PICC_REQALL                                       0x52       // find all the cards antenna area
+#define PICC_ANTICOLL                                     0x93       // anti-collision
+#define PICC_SELECTTAG                                    0x93       // election card
+#define PICC_AUTHENT1A                                    0x60       // authentication key A
+#define PICC_AUTHENT1B                                    0x61       // authentication key B
+#define PICC_READ                                         0x30       // Read Block
+#define PICC_WRITE                                        0xA0       // write block
+#define PICC_DECREMENT                                    0xC0       // debit
+#define PICC_INCREMENT                                    0xC1       // recharge
+#define PICC_RESTORE                                      0xC2       // transfer block data to the buffer
+#define PICC_TRANSFER                                     0xB0       // save the data in the buffer
+#define PICC_HALT                                         0x50       // Sleep
 
 // RC522 Registers
 // Page 0: Command and Status
@@ -105,6 +108,8 @@
 #define RC522_DUMMY                                       0x00        // Dummy byte
 #define RC522_MAX_LEN                                     16          // Buf len byte
 
+static void lowCSPin(void);
+static void highCSPin(void);
 static uint8_t rc522Compare(uint8_t* cardID, uint8_t* compareID);
 static void rc522WriteRaw(uint8_t ucAddress, uint8_t ucValue);
 static uint8_t rc522ReadRaw(uint8_t ucAddress);
@@ -116,8 +121,8 @@ static uint8_t rc522Anticoll(uint8_t* serNum);
 static void rc522CalculateCRC(uint8_t* pIndata, uint8_t len, uint8_t* pOutData);
 static uint8_t rc522SelectTag(uint8_t* serNum);
 static uint8_t rc522Auth(uint8_t authMode, uint8_t BlockAddr, uint8_t* Sectorkey, uint8_t* serNum);
-static uint8_t rc522Read(uint8_t blockAddr, uint8_t* recvData);
-static uint8_t rc522Write(uint8_t blockAddr, uint8_t* writeData);
+static uint8_t rc522ReadBlock(uint8_t blockAddr, uint8_t* recvData);
+static uint8_t rc522WriteBlock(uint8_t blockAddr, uint8_t* writeData);
 static void rc522Reset(void);
 static void rc522AntennaOn(void);
 static void rc522AntennaOff(void);
